6d6e9e2f9d0c7ec20929704f2dee7264f597b244
wongxuanyi <wong.xuanyi@hotmail.com>
2023-08-03 22:13:50 +0800

    Changed the file reader to the COH csv data file

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -11,7 +11,7 @@ def readcsv ():
     prev_amt = 0
 
 #Open CSV file 
-    with open('Profit & Loss (Surplus).csv') as csv_file:
+    with open('COH_MAB_data_files.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         linecount = 0
         highest_profit = 0
