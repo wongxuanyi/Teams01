4496e0bae24b5a157ef3c74e8f20bbdff2f94aa4
bingsu23 <132331161+bingsu23@users.noreply.github.com>
2023-08-12 18:41:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 import cash_on_hand, profit_loss, overheads
 
 def main():
-    
-    #calls each individual function
-    profit_loss_data = profit_loss.readCSV()
-    cash_on_hand_data = cash_on_hand.readCSV()
-    overhead_data = overheads.readCSV()
-
-    with open('summary_report.txt', 'w') as f:
-        #loop through the data
-        for y in cash_on_hand_data:
-            f.write('[CASH DEFICIT] day: ')
-            f.write(y[0])
-            f.write(',')
-
-            f.write('AMOUNT: USD')
-            f.write(str(y[1]))
-            f.write('/n')
-
-        for x in profit_loss_data:
-
-            f.write('[PROFIT INCREMENT] day: ')
-            f.write(x[0])
-            f.write(',')
-
-            f.write('AMOUNT: USD')
-            f.write(str(x[1]))
-            f.write('/n')
-
-    print('summary report done')
+    """
+    this function processes cash on hand, profit and loss and overheads and generates a summery report
+    """
+    cash_on_hand_data= cash_on_hand.readcsv()
+    Profit_loss_data = profit_loss.readcsv()
+    overhead_data = overheads.readcsv()
 
 main()
+
+
+
+
+
+
         
 
     
